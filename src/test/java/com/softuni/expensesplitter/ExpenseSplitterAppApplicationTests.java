8e5152652c7b03366c82ffd5364287e22637eb88
Kalin Karolev <karolevkalin@gmail.com>
2026-06-15 21:35:45 +0300

    #01 - Initial Spring Boot project setup

---
+++ src/test/java/com/softuni/expensesplitter/ExpenseSplitterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softuni.expensesplitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseSplitterAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
